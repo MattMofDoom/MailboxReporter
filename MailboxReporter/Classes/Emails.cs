82fe4e5bf6e9d3d76d889b9d91a39900db0f1d8e
MattMofDoom <matt.marlor@outlook.com>
2021-11-22 09:09:46 +1100

    Revert behaviour to local search filter and ExchangeService

--- MailboxReporter/Classes/Emails.cs
+++ MailboxReporter/Classes/Emails.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Lurgle.Logging;
 using Microsoft.Exchange.WebServices.Data;
@@ -7,53 +8,51 @@ namespace MailboxReporter.Classes
 {
     public static class Emails
     {
-        private static readonly SearchFilter.SearchFilterCollection LastHours = new SearchFilter.SearchFilterCollection(
-            LogicalOperator.And,
-            new SearchFilter.IsGreaterThan(ItemSchema.DateTimeReceived,
-                Config.LastTick.Subtract(TimeSpan.FromHours(Config.LastHours))));
-
-        private static readonly SearchFilter.SearchFilterCollection LastHoursUnread =
-            new SearchFilter.SearchFilterCollection(LogicalOperator.And,
-                new SearchFilter.IsGreaterThan(ItemSchema.DateTimeReceived,
-                    Config.LastTick.Subtract(TimeSpan.FromHours(Config.LastHours))),
-                new SearchFilter.IsEqualTo(EmailMessageSchema.IsRead, false));
-
-        private static readonly ExchangeService ExchangeServer =
-            new ExchangeService(ExchangeVersion.Exchange2013_SP1, TimeZoneInfo.Local)
-            {
-                UseDefaultCredentials = true,
-                AcceptGzipEncoding = Config.UseGzip,
-                WebProxy = null,
-                Credentials = !string.IsNullOrEmpty(Config.UserName) && !string.IsNullOrEmpty(Config.Password)
-                    ? new WebCredentials(Config.UserName, Config.Password)
-                    : new WebCredentials(),
-                Timeout = Config.ServerTimeout,
-                UserAgent = $"MailboxReporter/v{Logging.Config.AppVersion}"
-            };
-
         public static EmailResult ListEmails(string address)
         {
-            ExchangeServer.HttpHeaders.Clear();
-            ExchangeServer.HttpHeaders.Add("X-AnchorMailbox", address);
+            var lastHours = new SearchFilter.SearchFilterCollection(
+                LogicalOperator.And,
+                new SearchFilter.IsGreaterThan(ItemSchema.DateTimeReceived,
+                    Config.LastTick.Subtract(TimeSpan.FromHours(Config.LastHours))));
+
+            var lastHoursUnread =
+                new SearchFilter.SearchFilterCollection(LogicalOperator.And,
+                    new SearchFilter.IsGreaterThan(ItemSchema.DateTimeReceived,
+                        Config.LastTick.Subtract(TimeSpan.FromHours(Config.LastHours))),
+                    new SearchFilter.IsEqualTo(EmailMessageSchema.IsRead, false));
+
+            var exchangeServer =
+                new ExchangeService(ExchangeVersion.Exchange2013_SP1, TimeZoneInfo.Local)
+                {
+                    UseDefaultCredentials = true,
+                    AcceptGzipEncoding = Config.UseGzip,
+                    WebProxy = null,
+                    Credentials = !string.IsNullOrEmpty(Config.UserName) && !string.IsNullOrEmpty(Config.Password)
+                        ? new WebCredentials(Config.UserName, Config.Password)
+                        : new WebCredentials(),
+                    Timeout = Config.ServerTimeout,
+                    HttpHeaders = {new KeyValuePair<string, string>("X-AnchorMailbox", address)},
+                    UserAgent = $"MailboxReporter/v{Logging.Config.AppVersion}"
+                };
 
             if (!string.IsNullOrEmpty(Config.Url))
-                ExchangeServer.Url = new Uri(Config.Url);
+                exchangeServer.Url = new Uri(Config.Url);
             else
-                ExchangeServer.AutodiscoverUrl(address, RedirectionUrlValidationCallback);
+                exchangeServer.AutodiscoverUrl(address, RedirectionUrlValidationCallback);
 
             Log.Information().Add("[{Address:l}] Connecting ...", address);
             var startTime = DateTime.Now;
             var sqlClient = new SqlServerClient();
             sqlClient.Connect();
 
-            var result = new EmailResult {ExchangeUrl = ExchangeServer.Url.AbsoluteUri};
+            var result = new EmailResult {ExchangeUrl = exchangeServer.Url.AbsoluteUri};
 
             FindItemsResults<Item> fiResults;
 
             if (Config.IsDebug)
                 Log.Debug().Add("[{Address:l}] Connect to mailbox ...", address);
             var folderId = new FolderId(WellKnownFolderName.Inbox, new Mailbox(address));
-            var inboxFolder = Folder.Bind(ExchangeServer, folderId);
+            var inboxFolder = Folder.Bind(exchangeServer, folderId);
             var itemCount = inboxFolder.TotalCount;
             var unreadCount = inboxFolder.UnreadCount;
 
@@ -62,9 +61,9 @@ namespace MailboxReporter.Classes
             var pageCount = 1;
             var totalCount = 0;
 
-            result.HttpHeaders = ExchangeServer.HttpHeaders;
-            result.HttpResponseHeaders = ExchangeServer.HttpResponseHeaders;
-            result.ServerInfo = ExchangeServer.ServerInfo;
+            result.HttpHeaders = exchangeServer.HttpHeaders;
+            result.HttpResponseHeaders = exchangeServer.HttpResponseHeaders;
+            result.ServerInfo = exchangeServer.ServerInfo;
 
             Log.Information().AddProperty("Result", result, true).Add(
                 "[{Address:l}] Total emails found: {ItemCount}, unread: {UnreadCount} ...",
@@ -76,22 +75,22 @@ namespace MailboxReporter.Classes
                     Log.Debug().Add(
                         "[{Address:l}] Search for total items for past {Hours} hours from {LastReportTick:l} ...",
                         address, Config.LastHours, Config.LastTick.ToString("dd MMM yyyy HH:mm:ss"));
-                fiResults = inboxFolder.FindItems(LastHours, new ItemView(1));
+                fiResults = inboxFolder.FindItems(lastHours, new ItemView(1));
                 itemCount = fiResults.TotalCount;
 
                 if (Config.IsDebug)
                     Log.Debug().Add(
                         "[{Address:l}] Search for unread items for past {Hours} hours from {LastReportTick:l} ...",
                         address, Config.LastHours, Config.LastTick.ToString("dd MMM yyyy HH:mm:ss"));
-                fiResults = inboxFolder.FindItems(LastHoursUnread, new ItemView(1));
+                fiResults = inboxFolder.FindItems(lastHoursUnread, new ItemView(1));
                 unreadCount = fiResults.TotalCount;
 
                 iv = itemCount > 1000 ? new ItemView(1000) :
                     itemCount > 0 ? new ItemView(itemCount) : new ItemView(1);
 
-                result.HttpHeaders = ExchangeServer.HttpHeaders;
-                result.HttpResponseHeaders = ExchangeServer.HttpResponseHeaders;
-                result.ServerInfo = ExchangeServer.ServerInfo;
+                result.HttpHeaders = exchangeServer.HttpHeaders;
+                result.HttpResponseHeaders = exchangeServer.HttpResponseHeaders;
+                result.ServerInfo = exchangeServer.ServerInfo;
 
                 Log.Information().AddProperty("Result", result, true).Add(
                     "[{Address:l}] Retrieving last {Hours} hours from {LastReportTick:l} - Emails found: {ItemCount}, unread: {UnreadCount}  ...",
@@ -110,23 +109,60 @@ namespace MailboxReporter.Classes
             {
                 fiResults = Config.FirstRun
                     ? inboxFolder.FindItems(iv)
-                    : inboxFolder.FindItems(LastHours, iv);
+                    : inboxFolder.FindItems(lastHours, iv);
                 iv.Offset += fiResults.Items.Count;
 
-                result.HttpHeaders = ExchangeServer.HttpHeaders;
-                result.HttpResponseHeaders = ExchangeServer.HttpResponseHeaders;
-                result.ServerInfo = ExchangeServer.ServerInfo;
+                result.HttpHeaders = exchangeServer.HttpHeaders;
+                result.HttpResponseHeaders = exchangeServer.HttpResponseHeaders;
+                result.ServerInfo = exchangeServer.ServerInfo;
 
                 if (fiResults.Items.Count <= 0) continue;
                 Log.Information().AddProperty("Result", result).Add(
                     "[{Address:l}] Page {PageCount}, Emails found: {ItemCount} ...",
                     address, pageCount, fiResults.Items.Count);
 
-                totalCount += GetEmails(fiResults, address, ExchangeServer, sqlClient);
+                foreach (var emailRecord in fiResults.Items
+                    .Select(mailItem => EmailMessage.Bind(exchangeServer, mailItem.Id,
+                        new PropertySet(BasePropertySet.FirstClassProperties)))
+                    .Select(email => new EmailRecord
+                    {
+                        Id = email.Id.UniqueId,
+                        ConversationId = email.ConversationId.UniqueId,
+                        InternetMessageId = email.InternetMessageId,
+                        SentDate = email.DateTimeCreated,
+                        ReceivedDate = email.DateTimeReceived,
+                        CreatedDate = email.DateTimeCreated,
+                        ModifiedDate = email.LastModifiedTime,
+                        ModifiedName = email.LastModifiedName,
+                        BodyType = email.Body.BodyType,
+                        Body = Config.IncludePartialBody && !string.IsNullOrEmpty(email.Body)
+                            ? email.Body.Text.Length < Config.PartialBodyLength ? email.Body.Text :
+                            email.Body.Text.Substring(0, Config.PartialBodyLength)
+                            : "",
+                        Subject = email.Subject,
+                        FromName = email.Sender.Name,
+                        FromAddress = email.Sender.Address,
+                        ReplyToName = string.Join(",", email.ReplyTo.Select(x => x.Name).ToArray()),
+                        ReplyToAddress = string.Join(",", email.ReplyTo.Select(x => x.Address).ToArray()),
+                        ToName = string.Join(",", email.ToRecipients.Select(x => x.Name).ToArray()),
+                        ToAddress = string.Join(",", email.ToRecipients.Select(x => x.Address).ToArray()),
+                        CcName = string.Join(",", email.CcRecipients.Select(x => x.Name).ToArray()),
+                        CcAddress = string.Join(",", email.CcRecipients.Select(x => x.Address).ToArray()),
+                        IsRead = email.IsRead,
+                        Priority = email.Importance,
+                        AttachmentCount = email.Attachments.Where(x => !x.IsInline).Select(x => x.Name).Count(),
+                        Attachments = string.Join(",",
+                            email.Attachments.Where(x => !x.IsInline).Select(x => x.Name).ToArray()),
+                        Size = email.Size
+                    }))
+                {
+                    sqlClient.AddOrUpdate(address, emailRecord);
+                    totalCount++;
+                }
 
-                result.HttpHeaders = ExchangeServer.HttpHeaders;
-                result.HttpResponseHeaders = ExchangeServer.HttpResponseHeaders;
-                result.ServerInfo = ExchangeServer.ServerInfo;
+                result.HttpHeaders = exchangeServer.HttpHeaders;
+                result.HttpResponseHeaders = exchangeServer.HttpResponseHeaders;
+                result.ServerInfo = exchangeServer.ServerInfo;
 
                 Log.Information().AddProperty("Result", result, true).Add(
                     "[{Address:l}] Result set tally, Page {Page}: {CurrentCount} items of {TotalCount}, Duration: {Duration:N0} seconds",
@@ -146,53 +182,6 @@ namespace MailboxReporter.Classes
             sqlClient.Dispose();
 
             return result;
-        }
-
-        private static int GetEmails(FindItemsResults<Item> fiResults, string address, ExchangeService exchangeServer,
-            SqlServerClient sqlClient)
-        {
-            var totalCount = 0;
-            foreach (var emailRecord in fiResults.Items)
-            {
-                var email = EmailMessage.Bind(exchangeServer, emailRecord.Id,
-                    new PropertySet(BasePropertySet.FirstClassProperties));
-
-                sqlClient.AddOrUpdate(address, new EmailRecord
-                {
-                    Id = email.Id.UniqueId,
-                    ConversationId = email.ConversationId.UniqueId,
-                    InternetMessageId = email.InternetMessageId,
-                    SentDate = email.DateTimeCreated,
-                    ReceivedDate = email.DateTimeReceived,
-                    CreatedDate = email.DateTimeCreated,
-                    ModifiedDate = email.LastModifiedTime,
-                    ModifiedName = email.LastModifiedName,
-                    BodyType = email.Body.BodyType,
-                    Body = Config.IncludePartialBody && !string.IsNullOrEmpty(email.Body)
-                        ? email.Body.Text.Length < Config.PartialBodyLength ? email.Body.Text :
-                        email.Body.Text.Substring(0, Config.PartialBodyLength)
-                        : "",
-                    Subject = email.Subject,
-                    FromName = email.Sender.Name,
-                    FromAddress = email.Sender.Address,
-                    ReplyToName = string.Join(",", email.ReplyTo.Select(x => x.Name).ToArray()),
-                    ReplyToAddress = string.Join(",", email.ReplyTo.Select(x => x.Address).ToArray()),
-                    ToName = string.Join(",", email.ToRecipients.Select(x => x.Name).ToArray()),
-                    ToAddress = string.Join(",", email.ToRecipients.Select(x => x.Address).ToArray()),
-                    CcName = string.Join(",", email.CcRecipients.Select(x => x.Name).ToArray()),
-                    CcAddress = string.Join(",", email.CcRecipients.Select(x => x.Address).ToArray()),
-                    IsRead = email.IsRead,
-                    Priority = email.Importance,
-                    AttachmentCount = email.Attachments.Where(x => !x.IsInline).Select(x => x.Name).Count(),
-                    Attachments = string.Join(",",
-                        email.Attachments.Where(x => !x.IsInline).Select(x => x.Name).ToArray()),
-                    Size = email.Size
-                });
-
-                totalCount++;
-            }
-
-            return totalCount;
         }
 
         private static bool RedirectionUrlValidationCallback(string redirectionUrl)
